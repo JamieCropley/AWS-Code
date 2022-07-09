893a6f35405b3267ed6a40224ad2371fd5dd3e86
Jamie Cropley <jamie.cropley@protonmail.com>
2022-07-09 02:20:28 +0100

    initial commit

---
+++ csharp/source/repos/loops/question19.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops
+{
+    class question19
+    {
+        static void Main2(string[] args)
+        {
+            bool looper = true;
+            double sum = 0;
+            double count = 0;
+            while (looper == true)
+            {
+                sum = sum + Math.Pow(count, 2.0);
+                Console.WriteLine("count: {0}    sum: {1}", count, sum);
+                count++;
+            } while (count != 10);
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
