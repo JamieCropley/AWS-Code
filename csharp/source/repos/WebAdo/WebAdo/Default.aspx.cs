45ed2f2f526447b6af6fc8c2c83feacec982cd95
Jamie Cropley <jamie.cropley@protonmail.com>
2022-07-21 21:17:12 +0100

    Update (Deleted a lot of comments from original code as was causing issues)

--- csharp/source/repos/WebAdo/WebAdo/Default.aspx.cs
+++ csharp/source/repos/WebAdo/WebAdo/Default.aspx.cs
@@ -12,12 +12,14 @@ namespace WebAdo
 {
     public partial class Default : System.Web.UI.Page
     {
-        protected GridView GridView1;
         protected void Page_Load(object sender, EventArgs e)
         {
-            Response.Write("<center><h1>Read data from a database</h1></center><hr/>");
+            Response.Write("Hello World");
             Response.Write("<br/>");
+
             string s = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
+            Response.Write(s);
+
             SqlConnection con = new SqlConnection(s);
             string sqlString = "select * from customers";
             SqlCommand cmd = new SqlCommand(sqlString, con);
@@ -27,6 +29,27 @@ namespace WebAdo
             GridView1.DataBind();
             dr.Close();
             con.Close();
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write("<center><h1>Read data from a database</h1></center><hr/>");
+            Response.Write("<br/>");
+            String txtValue = TextBox1.Text;
+            string s = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
+            SqlConnection con = new SqlConnection(s);
+            string sqlString = "select * from customers where Country=@Country";
+            SqlCommand cmd = new SqlCommand(sqlString, con);
+            cmd.Parameters.AddWithValue("@Country", txtValue);
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            GridView1.DataSource = dr;
+            GridView1.DataBind();
+            dr.Close();
+            con.Close();
+
         }
     }
 }
