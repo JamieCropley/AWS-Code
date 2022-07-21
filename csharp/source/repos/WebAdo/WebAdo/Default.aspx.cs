db3371d81731382a631905c10e175136eed1d3df
Jamie Cropley <jamie.cropley@protonmail.com>
2022-07-21 20:42:29 +0100

    Update

---
+++ csharp/source/repos/WebAdo/WebAdo/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+namespace WebAdo
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected GridView GridView1;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("<center><h1>Read data from a database</h1></center><hr/>");
+            Response.Write("<br/>");
+            string s = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
+            SqlConnection con = new SqlConnection(s);
+            string sqlString = "select * from customers";
+            SqlCommand cmd = new SqlCommand(sqlString, con);
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            GridView1.DataSource = dr;
+            GridView1.DataBind();
+            dr.Close();
+            con.Close();
+        }
+    }
+}
