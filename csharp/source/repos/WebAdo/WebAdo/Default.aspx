d4c39493c1bda4227d5be9572848fbe9b4d45fb6
Jamie Cropley <jamie.cropley@protonmail.com>
2022-07-21 21:43:17 +0100

    Update

--- csharp/source/repos/WebAdo/WebAdo/Default.aspx
+++ csharp/source/repos/WebAdo/WebAdo/Default.aspx
@@ -8,10 +8,16 @@
 <h1>Read data from a database</h1>
 <form id="form1" runat="server">
 <div>
+    <br />
+    <br />
     <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    <br />
+    <br />
     <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
     <br />
     <br />
+    <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+    </asp:DropDownList>
     <br />
     <br />
 </div>
--- csharp/source/repos/WebAdo/WebAdo/Default.aspx.cs
+++ csharp/source/repos/WebAdo/WebAdo/Default.aspx.cs
@@ -14,27 +14,35 @@ namespace WebAdo
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Response.Write("Hello World");
-            Response.Write("<br/>");
+            if (!IsPostBack)
+            {
+                Response.Write("<center><h1>Read data from a database</h1></center><hr/>");
+                Response.Write("<br/>");
+                string s = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
+                SqlConnection con = new SqlConnection(s);
+                string sqlString = "select * from customers";
+                SqlCommand cmd = new SqlCommand(sqlString, con);
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                GridView1.DataSource = dr;
+                GridView1.DataBind();
+                dr.Close();
+                string sqlStringDropDownList = "select Country from customers";
+                SqlCommand cmd2 = new SqlCommand(sqlStringDropDownList, con);
+                SqlDataReader dr2 = cmd2.ExecuteReader();
+                while (dr2.Read() == true)
+                {
+                    DropDownList1.Items.Add(new ListItem(dr2["Country"].ToString(), dr2["Country"].ToString()));
+                }
+                dr2.Close();
+                con.Close();
 
-            string s = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
-            Response.Write(s);
-
-            SqlConnection con = new SqlConnection(s);
-            string sqlString = "select * from customers";
-            SqlCommand cmd = new SqlCommand(sqlString, con);
-            con.Open();
-            SqlDataReader dr = cmd.ExecuteReader();
-            GridView1.DataSource = dr;
-            GridView1.DataBind();
-            dr.Close();
-            con.Close();
-
-
+            }
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            
             Response.Write("<center><h1>Read data from a database</h1></center><hr/>");
             Response.Write("<br/>");
             String txtValue = TextBox1.Text;
@@ -49,7 +57,27 @@ namespace WebAdo
             GridView1.DataBind();
             dr.Close();
             con.Close();
+            
+        }
 
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                Response.Write("<br/>");
+                String txtValue = DropDownList1.SelectedValue.ToString();
+                string s = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
+                SqlConnection con = new SqlConnection(s);
+                string sqlString = "select * from customers where Country=@Country";
+                SqlCommand cmd = new SqlCommand(sqlString, con);
+                cmd.Parameters.AddWithValue("@Country", txtValue);
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                GridView1.DataSource = dr;
+                GridView1.DataBind();
+                dr.Close();
+                con.Close();
+            }
         }
     }
 }
